e27aaf31f1f0a500c983470816411a70e69f84b1
VinayPal90 <118743523+VinayPal90@users.noreply.github.com>
2026-02-06 12:57:29 +0530

    Update GoogleStrategy callbackURL configuration
    Fallback to environment variable for callback URL.

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -7,7 +7,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: "/auth/google/callback" || process.env.CALLBACK_URL
   },
   (accessToken, refreshToken, profile, done) => {
     // Yahan hum user profile return kar rahe hain
